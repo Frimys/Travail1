d882fde6104b94d1dc75bcbe788bbe7e5db5d673
Anthony Côté <1742246@cegepba.qc.ca>
2020-02-12 14:03:39 -0500

    Modiier Donnees (les cartes)

--- travail1/donnees.cpp
+++ travail1/donnees.cpp
@@ -1,4 +1,22 @@
 #include "donnees.h"
-
+Donnees::Donnees()
+{
+	string atout = "";
+	for (int numeroAtout= 0; numeroAtout < 4; numeroAtout++)
+	{
+		switch (numeroAtout) 
+		{
+		case 0: atout = "pique"; break;
+		case 1: atout = "coeur"; break;
+		case 2: atout = "trèfle"; break;
+		case 3: atout = "carreau"; break;
+			
+			for (int valeur = 1; valeur <= 13; valeur++)
+			{
+				LesCartes[(valeur + (numeroAtout * 13))].InitialiserCarte(valeur, atout);
+			}
+		}
+	}
+}
 
 
