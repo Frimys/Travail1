9313eb76605f60c7dce8f1aa85daa451250496d7
Anthony Côté <1742246@cegepba.qc.ca>
2020-02-12 13:35:42 -0500

    Modification des valeurs dans Joueur et les fonctions

--- travail1/joueur.cpp
+++ travail1/joueur.cpp
@@ -1,2 +1,47 @@
 #include "joueur.h"
-
+Joueur::Joueur()
+{
+	nom = "";
+	nombreVictoires = 0;
+	nombreDefaites = 0;
+	for (int i = 0; i < 26; i++)
+	{
+		mainDuJoueur[i] = NULL;
+	}
+}
+void Joueur::SetNom(string aNom)
+{
+	nom = aNom;
+}
+void Joueur::AjouterVictoire()
+{
+	nombreVictoires++;
+}
+void Joueur::AjouterDefaite()
+{
+	nombreDefaites++;
+}
+void Joueur::AjouterUneCarte(Carte* aCartePigee)
+{
+	bool carteAjoutee = false;
+	int nbCartes = 0;
+		while (carteAjoutee = false && nbCartes < 26)
+		{
+		if (mainDuJoueur[nbCartes] == NULL)
+			{
+			mainDuJoueur[nbCartes] = aCartePigee;
+			carteAjoutee = true;
+			}
+		else
+		{
+			nbCartes++;
+		}
+		}
+}
+void Joueur::EnleverLesCartes()
+{
+	for (int i = 0; i < 26; i++)
+	{
+		mainDuJoueur[i] = NULL;
+	}
+}
--- travail1/joueur.h
+++ travail1/joueur.h
@@ -8,6 +8,18 @@ using namespace std;
 const int maxCartesAJouer = 26;
 class Joueur
 {
+private:
+	string nom;
+	int nombreVictoires;
+	int nombreDefaites;
+	Carte* mainDuJoueur[26];
 
+public:
+	Joueur();
+	void SetNom(string);
+	void AjouterVictoire();
+	void AjouterDefaite();
+	void AjouterUneCarte(Carte*);
+	void EnleverLesCartes();
 };
 #endif
